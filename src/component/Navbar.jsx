d620b3ad2426901292e5f9c9664991479e51577b
syedhaseeb2005 <haseebsyed444@gmail.com>
2024-01-28 23:46:33 +0500

    Project Adding

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
-import { FaBars, FaTimes, FaGithub, FaLinkedinIn } from 'react-icons/fa';
-import { HiOutlineMail } from 'react-icons/hi'
-
-import { Link } from 'react-scroll';
+import React, { useState } from "react";
+import { FaBars, FaTimes, FaGithub, FaLinkedinIn } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
+import { motion } from "framer-motion";
+import { Link } from "react-scroll";
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
@@ -12,107 +12,122 @@ const Navbar = () => {
   };
 
   return (
-    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
+      <motion.div
+        initial={{ opacity: 0, y: -50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1 }}
+      >
+
       <div class="text-white font-bold p-3 bg-gray-800 rounded-md shadow-lg">
         Syed Haseeb
       </div>
-
-
-      {/* Menu */}
-      <ul  className='hidden md:flex space-x-6'>
-        <li className='bg-gray-800 rounded-md shadow-lg py-1 text-white font-bold'>
-          <Link to='home' smooth={true} duration={500}>
-            Home
-          </Link>
-        </li>
-        <li className='bg-gray-800 rounded-md shadow-lg py-1 text-white font-bold'>
-          <Link to='about' smooth={true} duration={500}>
-            About
-          </Link>
-        </li>
-        <li className='bg-gray-800 rounded-md shadow-lg py-1 text-white font-bold'>
-          <Link to='skills' smooth={true} duration={500}>
-            Skills
-          </Link>
-        </li>
-        <li className='bg-gray-800 rounded-md shadow-lg py-1 text-white font-bold'>
-          <Link to='work' smooth={true} duration={500}>
-            Work
-          </Link>
-        </li>
-        <li className='bg-gray-800 rounded-md shadow-lg py-1 text-white font-bold'>
-          <Link to='contact' smooth={true} duration={500}>
-            Contact
-          </Link>
-        </li>
-      </ul>
-
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0, y: -50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1 }}
+      >
+        {/* Menu */}
+        <ul className="hidden md:flex space-x-6">
+          <li className="bg-gray-800 rounded-md shadow-lg py-1 text-white font-bold">
+            <Link to="home" smooth={true} duration={500}>
+              Home
+            </Link>
+          </li>
+          <li className="bg-gray-800 rounded-md shadow-lg py-1 text-white font-bold">
+            <Link to="about" smooth={true} duration={500}>
+              About
+            </Link>
+          </li>
+          <li className="bg-gray-800 rounded-md shadow-lg py-1 text-white font-bold">
+            <Link to="skills" smooth={true} duration={500}>
+              Skills
+            </Link>
+          </li>
+          <li className="bg-gray-800 rounded-md shadow-lg py-1 text-white font-bold">
+            <Link to="work" smooth={true} duration={500}>
+              Work
+            </Link>
+          </li>
+          <li className="bg-gray-800 rounded-md shadow-lg py-1 text-white font-bold">
+            <Link to="contact" smooth={true} duration={500}>
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </motion.div>
       {/* Hamburger */}
-      <div onClick={handleNav} className='md:hidden z-10'>
+      <div onClick={handleNav} className="md:hidden z-10">
         {!nav ? <FaBars /> : <FaTimes />}
       </div>
 
       {/* Mobile Menu */}
       <ul
-        className={`md:hidden absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center transform ${nav ? 'translate-x-0' : '-translate-x-full'
-          } transition-transform ease-in-out duration-300 gap-3`}
+        className={`md:hidden absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center transform ${
+          nav ? "translate-x-0" : "-translate-x-full"
+        } transition-transform ease-in-out duration-300 gap-3`}
       >
-        <li className='py-6 px-6  bg-gray-800 rounded-md shadow-lg text-white font-bold'>
-          <Link onClick={handleNav} to='home' smooth={true} duration={500}>
+        <li className="py-6 px-6  bg-gray-800 rounded-md shadow-lg text-white font-bold">
+          <Link onClick={handleNav} to="home" smooth={true} duration={500}>
             Home
           </Link>
         </li>
-        <li className='py-6 px-6 bg-gray-800 rounded-md shadow-lg  text-white font-bold'>
-          <Link onClick={handleNav} to='about' smooth={true} duration={500}>
+        <li className="py-6 px-6 bg-gray-800 rounded-md shadow-lg  text-white font-bold">
+          <Link onClick={handleNav} to="about" smooth={true} duration={500}>
             About
           </Link>
         </li>
-        <li className='py-6 px-7  bg-gray-800 rounded-md shadow-lg text-white font-bold'>
-          <Link onClick={handleNav} to='skills' smooth={true} duration={500}>
+        <li className="py-6 px-7  bg-gray-800 rounded-md shadow-lg text-white font-bold">
+          <Link onClick={handleNav} to="skills" smooth={true} duration={500}>
             Skills
           </Link>
         </li>
-        <li className='py-6 px-7  bg-gray-800 rounded-md shadow-lg  text-white font-bold'>
-          <Link onClick={handleNav} to='work' smooth={true} duration={500}>
+        <li className="py-6 px-7  bg-gray-800 rounded-md shadow-lg  text-white font-bold">
+          <Link onClick={handleNav} to="work" smooth={true} duration={500}>
             Work
           </Link>
         </li>
-        <li className='py-6 px-5  bg-gray-800 rounded-md shadow-lg text-white font-bold'>
-          <Link onClick={handleNav} to='contact' smooth={true} duration={500}>
+        <li className="py-6 px-5  bg-gray-800 rounded-md shadow-lg text-white font-bold">
+          <Link onClick={handleNav} to="contact" smooth={true} duration={500}>
             Contact
           </Link>
         </li>
       </ul>
 
       {/* Social Icons */}
-      <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
+      <div className="hidden lg:flex fixed flex-col top-[35%] left-0">
         <ul>
-          <li className='w-[160px] h-[50px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600 rounded-md'>
-            <a className='flex justify-between items-center w-full text-gray-300'
-              href="https://www.linkedin.com/in/syed-haseeb-ahmed-80464025a/" >
+          <li className="w-[160px] h-[50px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600 rounded-md">
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              href="https://www.linkedin.com/in/syed-haseeb-ahmed-80464025a/"
+            >
               Linkedin <FaLinkedinIn size={30} />
             </a>
           </li>
         </ul>
         <ul>
-          <li className='w-[160px] h-[50px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333] rounded-md'>
-            <a className='flex justify-between items-center w-full text-gray-300'
-              href="https://github.com/syedhaseeb2005?tab=repositories" >
+          <li className="w-[160px] h-[50px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333] rounded-md">
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              href="https://github.com/syedhaseeb2005?tab=repositories"
+            >
               Github <FaGithub size={30} />
             </a>
           </li>
         </ul>
         <ul>
-          <li className='w-[160px] h-[50px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0] rounded-md'>
-            <a className='flex justify-between items-center w-full text-gray-300'
-              href="mailto:haseebsyed901@gmail.com" >
+          <li className="w-[160px] h-[50px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0] rounded-md">
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              href="mailto:haseebsyed901@gmail.com"
+            >
               Email <HiOutlineMail size={30} />
             </a>
           </li>
         </ul>
       </div>
-
-
     </div>
   );
 };
--- src/component/Work.jsx
+++ src/component/Work.jsx
@@ -1,54 +1,78 @@
-import React from 'react'
+import React from "react";
 // import img1 from '../assest/img-1.png'
 
 const Work = () => {
-    const projects = [
-        {
-          title: 'Project 1',
-          description: 'Social Media App with ReactJs Nodejs ExpressJs and Mongodb',
-          imageUrl: 'https://img.freepik.com/free-vector/antigravity-isometric-mobile-phone-with-messages-notifications_23-2148273359.jpg?size=626&ext=jpg&ga=GA1.1.140349720.1700509378&semt=ais', 
-        },
-        {
-          title: 'Project 2',
-          description: 'Ticket Booking App with ReactJs Nodejs ExpressJs and Mongodb',
-          imageUrl: 'https://img.freepik.com/free-vector/women-using-mobile-phone-booking-flight-ticket_1262-21308.jpg?size=626&ext=jpg&ga=GA1.1.140349720.1700509378&semt=ais',
-        },
-        {
-          title: 'Project 3',
-          description: 'Attendance App with ReactJs Nodejs ExpressJs and Mongodb',
-          imageUrl: 'https://img.freepik.com/free-vector/appointment-booking-with-woman-smartphone_23-2148557484.jpg?size=626&ext=jpg&ga=GA1.1.140349720.1700509378&semt=ais',
-        },
-        {
-          title: 'Project 4',
-          description: 'E-commerce App with ReactJs Nodejs ExpressJs and Mongodb',
-          imageUrl: require('../assest/img.png')
-        },
-        {
-          title: 'Project 5',
-          description: 'Weather App with ReactJs',
-          imageUrl: require('../assest/img-1.png'),
-        },
-      ];
-    return (
-        <section name='work' className="py-16 bg-[#0a192f] text-gray-300 w-full h-full ">
+  const projects = [
+    {
+      title: "Project 1",
+      description: "Blog App with ReactJs Nodejs ExpressJs and Mongodb",
+      imageUrl: require("../assest/blogImg.png"),
+      projectLink: "https://github.com/syedhaseeb2005/Blog-App-Mern-Stack",
+    },
+    {
+      title: "Project 2",
+      description:
+        "Netflix with ReactJs & Authentication with Nodejs & MongoDB",
+      imageUrl: require("../assest/netflix img.png"),
+      projectLink: "https://github.com/syedhaseeb2005/Netflix-clone",
+    },
+    {
+      title: "Project 3",
+      description: "Social Media App with ReactJs Nodejs ExpressJs and Mongodb",
+      imageUrl:
+        "https://img.freepik.com/free-vector/appointment-booking-with-woman-smartphone_23-2148557484.jpg?size=626&ext=jpg&ga=GA1.1.140349720.1700509378&semt=ais",
+        projectLink : "https://github.com/syedhaseeb2005/Social-Media-React"
+      },
+    {
+      title: "Project 4",
+      description: "E-commerce App with ReactJs Nodejs ExpressJs and Mongodb",
+      imageUrl: require("../assest/img.png"),
+      projectLink : "https://github.com/syedhaseeb2005/E-commerce-Web"
+    },
+    {
+      title: "Project 5",
+      description: "Movie App using with ReactJs Redux ",
+      imageUrl: require("../assest/download.jpeg"),
+      projectLink : "https://github.com/syedhaseeb2005/React-Redux-MovieApp"
+    },
+  ];
+  return (
+    <section
+      name="work"
+      className="py-16 bg-[#0a192f] text-gray-300 w-full h-full "
+    >
       <div className="container mx-auto max-w-[1000px] items-start  p-4 flex flex-col justify-center w-full h-full">
         <div>
-          <h2 className="text-4xl font-bold inline border-b-2 border-b-pink-600">My Work</h2>
-          <p className='py-4'>Check out some of my recent work</p>
+          <h2 className="text-4xl font-bold inline border-b-2 border-b-pink-600">
+            My Work
+          </h2>
+          <p className="py-4">Check out some of my recent work</p>
         </div>
         <div className="w-full cursor-pointer grid grid-cols-2 sm:grid-cols-5 gap-4 text-center py-8">
           {projects.map((project, index) => (
-            <div key={index} className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 p-2">
-              <img src={project.imageUrl} alt={project.title} className="mb-4 object-cover rounded-md shadow-md w-[190px] h-[150px]"/>
-              <h3 className="text-xl font-semibold mb-2">{project.title}</h3>
-              <p className="text-gray-600">{project.description}</p>
-            </div>
+            <a
+              href={project.projectLink}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <div
+                key={index}
+                className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 p-2"
+              >
+                <img
+                  src={project.imageUrl}
+                  alt={project.title}
+                  className="mb-4 object-cover rounded-md shadow-md w-[190px] h-[150px]"
+                />
+                <h3 className="text-xl font-semibold mb-2">{project.title}</h3>
+                <p className="text-gray-600">{project.description}</p>
+              </div>
+            </a>
           ))}
         </div>
       </div>
     </section>
+  );
+};
 
-    )
-}
-
-export default Work
+export default Work;
